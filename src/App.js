d49ad45a76114ad5e737a02410dc849c419d9a9c
phucnt31 <nguyenthienphuc.02@gmail.com>
2022-09-22 21:28:53 +0700

    remove some file and add new routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Admin from "./layouts/Admin";
+import Auth from "./layouts/Auth";
+import Landing from "./views/Landing";
+import Profile from "./views/Profile";
+import Dashboard from "./views/admin/Dashboard";
+import Settings from "./views/admin/Settings";
+import Tables from "./views/admin/Tables";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/styles/tailwind.css";
+import Login from "./views/auth/Login";
+import Register from "./views/auth/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* add routes with layouts */}
+        <Route index element={<Admin />} />
+        <Route path="/admin" element={<Admin />}>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/settings" element={<Settings />} />
+          <Route path="/admin/tables" element={<Tables />} />
+        </Route>
+        <Route path="/auth" element={<Auth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        {/* add routes without layouts */}
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/profile" element={<Profile />} />
+
+        {/* add redirect for first page */}
+        <Route path="*" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
