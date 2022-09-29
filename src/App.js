31cd283009294ab49b6af30ceb0305d59b5834f3
HT-Dat <hotiendat661@gmail.com>
2022-09-29 13:27:30 +0700

    Comment out obsolete css

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Dashboard from "./views/admin/Dashboard";
 import Settings from "./views/admin/Settings";
 import Tables from "./views/admin/Tables";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import "./assets/styles/tailwind.css";
+// import "./assets/styles/tailwind.css";
 import Login from "./views/auth/Login";
 import Register from "./views/auth/Register";
 import "react-toastify/dist/ReactToastify.css";
