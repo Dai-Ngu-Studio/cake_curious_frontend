781d2a4322bfb9999fa6d3f1c1754933e45ea9a2
phucnt31 <nguyenthienphuc.02@gmail.com>
2022-10-06 23:22:46 +0700

    Added placeholder image and some ultils function

---
+++ src/ultils/SortingOptions.js
@@ -0,0 +1,67 @@
+export const ProductSortingOptions = [
+  {
+    name: "All",
+    value: "All",
+  },
+  {
+    name: "a-z",
+    value: "AscName",
+  },
+  {
+    name: "z-a",
+    value: "DescName",
+  },
+  {
+    name: "lowest",
+    value: "AscPrice",
+  },
+  {
+    name: "highest",
+    value: "DescPrice",
+  },
+];
+
+export const AccountSortingOptions = [
+  {
+    name: "All",
+    value: "All",
+  },
+  {
+    name: "a-z",
+    value: "AscDisplayName",
+  },
+  {
+    name: "z-a",
+    value: "DescDisplayName",
+  },
+];
+
+export const StoreSortingOptions = [
+  {
+    name: "All",
+    value: "All",
+  },
+  {
+    name: "a-z",
+    value: "AscName",
+  },
+  {
+    name: "z-a",
+    value: "DescName",
+  },
+];
+
+export const ReportSortingOptions = [
+  {
+    name: "All",
+    value: "All",
+  },
+  {
+    name: "a-z",
+    value: "AscTitle",
+  },
+  {
+    name: "z-a",
+    value: "DescTitle",
+  },
+];
