7ed0f3dac6dd9fee674b642075ef93ddd9db6ea4
phucnt31 <nguyenthienphuc.02@gmail.com>
2022-10-10 16:14:25 +0700

    Push error routing so Dat can fix it

--- src/ultils/ProtectedRoute.js
+++ src/ultils/ProtectedRoute.js
@@ -1,14 +1,35 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
-  const { token } = useSelector((store) => store.user);
+  const { token, user } = useSelector((store) => store.user);
 
+  // if (token) {
+  //   if (user) {
+  //     // for (let index = 0; index < user.hasRoles.length; index++) {
+  //     //   const element = user.hasRoles[index];
+  //     //   // if (element.roleId === 0) {
+  //     //   //   return <Navigate to="/admin/admin-dashboard" />;
+  //     //   // } else if (element.roleId === 1) {
+  //     //   //   console.log(element.roleId);
+  //     //   //   console.log("hi");
+  //     //   //   return <Navigate to="/staff/staff-dashboard" />;
+  //     //   // } else if (element.roleId === 2) {
+  //     //   //   return <Navigate to="/store/store-dashboard" />;
+  //     //   // }
+  //     //   //return <Navigate to="/staff/staff-dashboard" />;
+  //     // }
+  //     return <Navigate to="/staff/staff-dashboard" />;
+  //   }
+  // }
   if (!token) {
     return <Navigate to="/" />;
+  } else {
+    if (user) {
+      return children;
+    }
   }
-  return children;
 };
 
 export default ProtectedRoute;
