bb0a7bc69ab52f5c12216389fd7c56a6f2306731
HT-Dat <hotiendat661@gmail.com>
2022-11-03 02:17:33 +0700

    Remove old code

--- src/components/Dropdowns/UserDropdown.js
+++ src/components/Dropdowns/UserDropdown.js
@@ -6,19 +6,6 @@ import { clearStore } from "../../features/users/userSlice";
 import { Link } from "react-router-dom";
 import { FaSignOutAlt, FaUser } from "react-icons/fa";
 const UserDropdown = () => {
-  // dropdown props
-  // const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
-  // const btnDropdownRef = React.createRef();
-  // const popoverDropdownRef = React.createRef();
-  // const openDropdownPopover = () => {
-  //   createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-  //     placement: "bottom-start",
-  //   });
-  //   setDropdownPopoverShow(true);
-  // };
-  // const closeDropdownPopover = () => {
-  //   setDropdownPopoverShow(false);
-  // };
   const [isDropDownExpanded, setToggleDropDown] = useState(false);
   const dispatch = useDispatch();
   const [routing, setRouting] = useState("");
