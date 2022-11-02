4e04ad284f2d5b0125ab0b1f438bdbee70fadcfc
HT-Dat <hotiendat661@gmail.com>
2022-11-03 02:18:02 +0700

    Fix table dropdown selection

--- src/components/Dropdowns/TableDropdown.js
+++ src/components/Dropdowns/TableDropdown.js
@@ -1,49 +1,33 @@
-import React from "react";
-import { createPopper } from "@popperjs/core";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
 const TableDropdown = ({ link, setUpdate }) => {
   // dropdown props
-  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
-  const btnDropdownRef = React.createRef();
-  const popoverDropdownRef = React.createRef();
-  const openDropdownPopover = () => {
-    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
-      placement: "left-start",
-    });
-    setDropdownPopoverShow(true);
-  };
-  const closeDropdownPopover = () => {
-    setDropdownPopoverShow(false);
-  };
+  const [dropdownPopoverShow, setDropdownPopoverShow] = useState(false);
   const dispatch = useDispatch();
+
+  const [isDropDownTableExpanded, setToggleTableDropDown] = useState(false);
 
   return (
     <>
-      <a
-        className="text-blueGray-500 py-1 px-3"
-        href="#pablo"
-        ref={btnDropdownRef}
-        onClick={(e) => {
-          e.preventDefault();
-          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+      <div
+        className="text-blueGray-500 py-1 px-3 hover:cursor-pointer"
+        onClick={() => {
+          setToggleTableDropDown(!isDropDownTableExpanded);
         }}
       >
         <i className="fas fa-ellipsis-v"></i>
-      </a>
+      </div>
       <div
-        ref={popoverDropdownRef}
         className={
-          (dropdownPopoverShow ? "block " : "hidden ") +
-          "bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+          (isDropDownTableExpanded ? "absolute " : "hidden ") +
+          "right-14 top-16 z-10 list-none rounded shadow-2xl"
         }
       >
         <Link
           to={link}
-          className={
-            "text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          }
+          className="bg-white text-base z-50 rounded shadow-lg hover:bg-sky-100"
           onClick={() => dispatch(setUpdate)}
         >
           Update
