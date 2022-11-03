49eb6892364c95fc0f80a21835865b58728b35bc
HT-Dat <hotiendat661@gmail.com>
2022-11-03 22:56:21 +0700

    Rework sidebar UI

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,12 +4,13 @@ import { Link, NavLink } from "react-router-dom";
 
 import NotificationDropdown from "../Dropdowns/NotificationDropdown";
 import UserDropdown from "../Dropdowns/UserDropdown";
-
+import logoText from "../../assets/img/cake-curious-logo-text-line.png";
+import userTemp from "../../assets/img/user-sidebar-temp.png";
 export default function Sidebar({ props }) {
   const [collapseShow, setCollapseShow] = React.useState("hidden");
   return (
-    <>
-      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
+    <div className="">
+      <nav className="bg-emerald-600 rounded-r-3xl md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
         <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
           {/* Toggler */}
           <button
@@ -21,10 +22,10 @@ export default function Sidebar({ props }) {
           </button>
           {/* Brand */}
           <Link
-            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
+            className="font-bold text-white text-2xl text-center pt-5"
             to={props.dashboardLink}
           >
-            Cake Curious
+            <img src={logoText} className=""></img>
           </Link>
           {/* User */}
           <ul className="md:hidden items-center flex flex-wrap list-none">
@@ -43,14 +44,14 @@ export default function Sidebar({ props }) {
             }
           >
             {/* Collapse header */}
-            <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
+            <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid">
               <div className="flex flex-wrap">
                 <div className="w-6/12">
                   <Link
                     className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                     to={props.dashboardLink}
                   >
-                    Cake Curios
+                    Cake Curious
                   </Link>
                 </div>
                 <div className="w-6/12 flex justify-end">
@@ -64,34 +65,26 @@ export default function Sidebar({ props }) {
                 </div>
               </div>
             </div>
-            {/* Form */}
-            {/* <form className="mt-6 mb-4 md:hidden">
-              <div className="mb-3 pt-0">
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="px-3 py-2 h-12 border border-solid  border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
-                />
+            <div className="flex items-center justify-center">
+              <div className="bg-slate-300 rounded-full w-32 h-32 overflow-hidden">
+                <img src={userTemp}></img>
               </div>
-            </form> */}
-
-            {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
-            {/* Heading */}
-            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
+            </div>
+            <div className="text-center text-white font-bold py-10">
+              Chào mừng trở lại, Đạt
+            </div>
+            <h6 className="md:min-w-full text-slate-300 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
               {props.title}
             </h6>
             {/* Navigation */}
 
-            <ul className="md:flex-col md:min-w-full flex flex-col list-none">
+            <ul className="md:flex-col md:min-w-full flex flex-col">
               {props.links.map((item) => (
                 <li className="items-center" key={item.name}>
                   <NavLink
                     className={({ isActive }) =>
-                      "text-xs uppercase py-3 font-bold block " +
-                      (isActive
-                        ? "text-lightBlue-500 hover:text-lightBlue-600"
-                        : "text-blueGray-700 hover:text-blueGray-500")
+                      "text-xs uppercase py-3 font-bold block hover:text-emerald-600 hover:bg-white pl-5 m-1 rounded-lg text-white " +
+                      (isActive ? "bg-emerald-500" : "")
                     }
                     to={item.linkTo}
                   >
@@ -101,10 +94,9 @@ export default function Sidebar({ props }) {
               ))}
             </ul>
             {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
