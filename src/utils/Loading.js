48614260665aa97c29615213038c67b65a365c2f
HT-Dat <hotiendat661@gmail.com>
2022-11-13 00:05:58 +0700

    Make loading spinner bigger, height full screen

--- src/utils/Loading.js
+++ src/utils/Loading.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="text-center">
+    <div className="text-center h-screen flex items-center justify-center">
       <svg
         role="status"
-        className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="inline w-44 h-44 mr-2 text-gray-200 animate-spin fill-green-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
