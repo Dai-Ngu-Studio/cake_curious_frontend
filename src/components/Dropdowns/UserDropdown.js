e9dadc1ee5d51440944ecbfa6f21875647a27003
HT-Dat <hotiendat661@gmail.com>
2022-11-12 16:47:54 +0700

    Show name on navbar, improve ui

--- src/components/Dropdowns/UserDropdown.js
+++ src/components/Dropdowns/UserDropdown.js
@@ -3,7 +3,7 @@ import ProfileImage from "../../assets/img/user.png";
 import { useDispatch, useSelector } from "react-redux";
 import { clearStore } from "../../features/users/userSlice";
 import { Link } from "react-router-dom";
-import { FaSignOutAlt, FaUser } from "react-icons/fa";
+import { FaSignOutAlt, FaUser, FaAngleDown } from "react-icons/fa";
 const UserDropdown = () => {
   const [isDropDownExpanded, setToggleDropDown] = useState(false);
   const dispatch = useDispatch();
@@ -28,11 +28,10 @@ const UserDropdown = () => {
       }
     }
   }, []);
-
   return (
     <>
       <button
-        className="text-blueGray-500 block"
+        className="flex justify-between items-center hover:scale-105 "
         type="button"
         onClick={() => {
           setToggleDropDown(!isDropDownExpanded);
@@ -48,32 +47,34 @@ const UserDropdown = () => {
             />
           </span>
         </div>
+        <div className="pl-2">{user.displayName}</div>
+        <div>
+          <FaAngleDown className="text-2xl pl-2" />
+        </div>
       </button>
       <div
         className={
           (isDropDownExpanded ? "absolute " : "hidden ") +
-          "right-14 top-16 z-10 w-fit mt-4 bg-white border border-gray-100 rounded-md shadow-slate-900/25 shadow-xl"
+          "right-14 top-14 z-10 w-32 mt-4 bg-white border border-gray-100 rounded-md shadow-slate-900/25 shadow-xl"
         }
       >
         <Link
           to={routing}
-          className={
-            "text-sm m-1 p-2  flex text-blueGray-700 justify-center items-center gap-2 hover:bg-sky-300 rounded-md"
-          }
+          className="text-sm m-1 p-2 flex justify-center items-center gap-2 hover:bg-green-300 rounded-md"
+
           // onClick={() => setToggleDropDown(false)}
         >
           <FaUser className="flex justify-center items-center" />
           <p>Profile</p>
         </Link>
-        <div className="h-0 my-1 border border-solid border-blueGray-100" />
-        <button
-          type="button"
-          className="text-sm m-1 p-2 text-blueGray-700 flex gap-2 justify-center items-center hover:bg-sky-300 rounded-md"
+        <div className="h-0 my-1 border border-solid" />
+        <div
+          className="text-sm m-1 p-2 flex justify-center items-center gap-2 hover:bg-green-300 rounded-md cursor-pointer"
           onClick={() => dispatch(clearStore("Logging out..."))}
         >
           <FaSignOutAlt className="flex justify-center items-center" />
           Logout
-        </button>
+        </div>
       </div>
     </>
   );
