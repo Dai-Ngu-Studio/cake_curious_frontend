db800ad9a9227fdfd9ab6ac57a358c84632c0fc6
HT-Dat <hotiendat661@gmail.com>
2022-11-16 02:02:02 +0700

    Create gray wrapper for modal

---
+++ src/components/Cards/ModalWrapper.js
@@ -0,0 +1,17 @@
+import StoreViewModal from "./StoreViewModal";
+import { BsXLg } from "react-icons/bs";
+export default function ModalWrapper({ setOpenModal, store }) {
+  return (
+    <div>
+      <div
+        className="fixed bg-black/75 w-full h-full top-0 left-0"
+        onClick={() => {
+          setOpenModal(false);
+        }}
+      >
+        <BsXLg className="text-5xl fixed right-5 top-5 rounded-full hover:bg-white/25 p-3 text-white cursor-pointer" />
+      </div>
+      <StoreViewModal store={store} />
+    </div>
+  );
+}
