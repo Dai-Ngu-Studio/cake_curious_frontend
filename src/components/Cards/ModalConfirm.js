ca60b72f5b48f30f94e4712f7233b4182957d2f1
HT-Dat <hotiendat661@gmail.com>
2022-11-16 20:19:19 +0700

    Change prop store to viewObject

--- src/components/Cards/ModalConfirm.js
+++ src/components/Cards/ModalConfirm.js
@@ -1,10 +1,9 @@
 import StatusCard from "./StatusCard";
 export default function ModalConfirm({
-  store,
-  changeStoreStatus,
+  viewObject,
+  changeStatus,
   setOpenModal,
 }) {
-  console.log(store.status);
   return (
     <div className="fixed top-1/2 left-1/2 -translate-x-1/3 -translate-y-1/2">
       <div className="p-6 text-center bg-white rounded-lg px-16">
@@ -25,7 +24,7 @@ export default function ModalConfirm({
         </svg>
         <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
           Bạn muốn thay đổi trạng thái này sang
-          {store.status === 1 ? (
+          {viewObject.status === 1 ? (
             <StatusCard
               text="Hoạt động"
               backgroundColor="bg-green-200"
@@ -43,7 +42,7 @@ export default function ModalConfirm({
           type="button"
           className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
           onClick={() => {
-            changeStoreStatus(store.id, store.status);
+            changeStatus(viewObject.id, viewObject.status);
             setOpenModal(false);
           }}
         >
