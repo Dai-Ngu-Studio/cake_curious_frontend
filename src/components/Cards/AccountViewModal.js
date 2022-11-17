a58d74b4dbfee68bf4a262c0ff362cf68a73b574
HT-Dat <hotiendat661@gmail.com>
2022-11-17 18:22:39 +0700

    Change AccountViewModal to get FULL info from API

--- src/components/Cards/AccountViewModal.js
+++ src/components/Cards/AccountViewModal.js
@@ -1,47 +1,86 @@
-import Shop from "../../assets/img/no-store.png";
+import { useEffect } from "react";
+import NoImg from "../../assets/img/user.png";
+import { useDispatch, useSelector } from "react-redux";
+import { getSingleAccount } from "../../features/accounts/accountSlice";
+import Loading from "../../utils/Loading";
 
-export default function AccountViewModal({ account }) {
-  console.log(account);
+export default function AccountViewModal({ id }) {
+  const { user, isModalAccountLoading } = useSelector((slice) => slice.account);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getSingleAccount({ userId: id }));
+  }, []);
+  if (isModalAccountLoading) {
+    return <Loading />;
+  }
   return (
     <div className="fixed top-1/2 left-1/2 -translate-x-1/3 -translate-y-1/2 ">
-      <div className="flex">
-        {/* {account.email} */}
-        {/* <img
-          className="relative min-w-48 max-w-md scale-110 bg-white rounded-xl object-contain border"
-          src={store.photoUrl || Shop}
+      <div className="block item bg-white p-2 rounded-md">
+        <img
+          className="rounded-full object-contain border ml-auto mr-auto w-36 h-36"
+          src={user.photoUrl || NoImg}
+          referrerPolicy="no-referrer"
         />
-        <div className="pl-7 p-5 bg-gray-100 rounded-xl">
+        <div className="p-5 rounded-xl">
           <div className="flex items-center gap-5 ">
-            <div className="w-28 text-right">Tên cửa hàng </div>
+            <div className="w-20 text-right">Tên đại diện</div>
             <div className="w-96 p-2 rounded-md border-gray-300 border">
-              {store.name}
+              {user.displayName}
             </div>
           </div>
           <div className="flex items-center gap-5 my-3">
-            <div className="w-28 text-right">Tên chủ nhân </div>
+            <div className="w-20 text-right">Tên đầy đủ</div>
             <div className="w-96 p-2 rounded-md border-gray-300 border">
-              {store.user?.displayName || "Anonymous User"}
+              {user.fullName || <div className="text-gray-500">Unknown</div>}
             </div>
           </div>
           <div className="flex items-center gap-5">
-            <div className="w-28 text-right">Mô tả</div>
-            <div className="max-h-24 w-96 rounded-md overflow-scroll border-gray-300 border p-2">
-              {store.description}
+            <div className="w-20 text-right">Ngày đăng ký CCCD</div>
+            <div className="w-96 p-2 rounded-md border-gray-300 border">
+              {user.citizenshipDate || (
+                <div className="text-gray-500">Unknown</div>
+              )}
             </div>
           </div>
           <div className="flex items-center gap-5 my-3">
-            <div className="w-28 text-right">Địa chỉ </div>
+            <div className="w-20 text-right">Số CCCD</div>
             <div className="w-96 p-2 rounded-md border-gray-300 border">
-              {store.address}
+              {user.citizenshipNumber || (
+                <div className="text-gray-500">Unknown</div>
+              )}
             </div>
           </div>
           <div className="flex items-center gap-5">
-            <div className="w-28 text-right">Đánh giá </div>
+            <div className="w-20 text-right">Ngày tạo tài khoản </div>
             <div className="w-96 p-2 rounded-md border-gray-300 border">
-              {store.rating}
+              {user.createdDate || <div className="text-gray-500">Unknown</div>}
             </div>
           </div>
-        </div> */}
+          <div className="flex items-center gap-5 my-3">
+            <div className="w-20 text-right">Ngày sinh </div>
+            <div className="w-96 p-2 rounded-md border-gray-300 border">
+              {user.dateOfBirth || <div className="text-gray-500">Unknown</div>}
+            </div>
+          </div>
+          <div className="flex items-center gap-5">
+            <div className="w-20 text-right">Email</div>
+            <div className="w-96 p-2 rounded-md border-gray-300 border">
+              {user.email || <div className="text-gray-500">Unknown</div>}
+            </div>
+          </div>
+          <div className="flex items-center gap-5 my-3">
+            <div className="w-20 text-right">Giới tính</div>
+            <div className="w-96 p-2 rounded-md border-gray-300 border">
+              {user.gender || <div className="text-gray-500">Unknown</div>}
+            </div>
+          </div>
+          <div className="flex items-center gap-5">
+            <div className="w-20 text-right">Mã số</div>
+            <div className="w-96 p-2 rounded-md border-gray-300 border">
+              {user.id || <div className="text-gray-500">Unknown</div>}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
