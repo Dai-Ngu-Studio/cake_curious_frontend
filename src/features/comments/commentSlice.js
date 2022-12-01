5bbe13f797395cb92d94061a79e4bb169e2de22e
HT-Dat <hotiendat661@gmail.com>
2022-12-02 02:42:33 +0700

    add get and take down comment to slice

--- src/features/comments/commentSlice.js
+++ src/features/comments/commentSlice.js
@@ -1,21 +1,35 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
-import { getAllReportedCommentsThunk } from "./commentThunk";
+import {
+  getAllReportedCommentsThunk,
+  getCommentThunk,
+  deleteCommentThunk,
+} from "./commentThunk";
 const initialState = {
-  isLoading: true,
+  isCommentsLoading: true,
+  isCommentDoneUpdating: true,
   reportedComments: [],
+  comment: null,
   isModalLoading: true,
   page: 1,
   size: 10,
   search: "",
   sort: "All",
-  filter: "All",
+  filter: "Active",
   totalPage: 1,
 };
 
 export const getAllReportedComments = createAsyncThunk(
   "recipe/getComments",
   getAllReportedCommentsThunk
+);
+export const getComment = createAsyncThunk(
+  "comment/getComment",
+  getCommentThunk
+);
+export const deleteComment = createAsyncThunk(
+  "comment/deleteComment",
+  deleteCommentThunk
 );
 // export const getSingleAccount = createAsyncThunk(
 //   "account/getSingleAccount",
@@ -45,16 +59,35 @@ const commentSlice = createSlice({
   },
   extraReducers: {
     [getAllReportedComments.pending]: (state) => {
-      state.isLoading = true;
+      state.isCommentsLoading = true;
     },
     [getAllReportedComments.fulfilled]: (state, { payload }) => {
-      state.isLoading = false;
+      state.isCommentsLoading = false;
       state.reportedComments = payload.comments;
       state.totalPage = payload.totalPage;
     },
     [getAllReportedComments.rejected]: (state, { payload }) => {
-      state.isLoading = false;
+      state.isCommentsLoading = false;
       toast.error(payload);
+    },
+    [getComment.pending]: (state) => {
+      state.isCommentsLoading = true;
+    },
+    [getComment.fulfilled]: (state, { payload }) => {
+      state.isCommentsLoading = false;
+      state.comment = payload;
+    },
+    [getComment.rejected]: (state, { payload }) => {
+      state.isCommentsLoading = false;
+    },
+    [deleteComment.pending]: (state) => {
+      state.isCommentDoneUpdating = false;
+    },
+    [deleteComment.fulfilled]: (state, { payload }) => {
+      state.isCommentDoneUpdating = true;
+    },
+    [deleteComment.rejected]: (state, { payload }) => {
+      state.isCommentDoneUpdating = true;
     },
   },
 });
