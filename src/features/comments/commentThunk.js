a85d4a25923a19712af1db5cfc27c9d1a2d66a4f
HT-Dat <hotiendat661@gmail.com>
2022-12-02 02:43:33 +0700

    Add getCommentThunk and fixed filter value

--- src/features/comments/commentThunk.js
+++ src/features/comments/commentThunk.js
@@ -2,12 +2,28 @@ import customFetch, { checkForUnauthorizedResponse } from "../../utils/axios";
 
 export const getAllReportedCommentsThunk = async (_, thunkAPI) => {
   const { search, page, size, sort, filter } = thunkAPI.getState().account;
-  let url = `api/comments/is-reported?sort=${sort}&filter=${filter}&page=${page}&size=${size}`;
+  let url = `api/comments/is-reported?sort=${sort}&filter=Active&page=${page}&size=${size}`;
   if (search) {
     url += `&search=${search}`;
   }
   try {
     const resp = await customFetch.get(url);
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
+export const getCommentThunk = async ({ id }, thunkAPI) => {
+  try {
+    const resp = await customFetch.get(`/api/comments/${id}`);
+    return resp.data;
+  } catch (error) {
+    return checkForUnauthorizedResponse(error, thunkAPI);
+  }
+};
+export const deleteCommentThunk = async ({ id }, thunkAPI) => {
+  try {
+    const resp = await customFetch.delete(`/api/comments/take-down/${id}`);
     return resp.data;
   } catch (error) {
     return checkForUnauthorizedResponse(error, thunkAPI);
