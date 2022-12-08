bf1199652d755cf2b6b9aed5ccdd8bd6f61f3f7b
HT-Dat <hotiendat661@gmail.com>
2022-12-08 15:45:37 +0700

    Change UI of status card to less likely a button

--- src/components/Cards/StatusCard.js
+++ src/components/Cards/StatusCard.js
@@ -2,7 +2,7 @@ export default function StatusCard({ text, backgroundColor, dotColor }) {
   return (
     <div
       className={
-        "flex items-center justify-center  p-2 rounded-md " + backgroundColor
+        "flex items-center justify-center  p-2 rounded-full " + backgroundColor
       }
     >
       <div className={"mr-3 w-3 h-3 rounded-full bg-gr " + dotColor}></div>
