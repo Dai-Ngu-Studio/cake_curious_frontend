90e31ce85c4f74a8ff1522e8af350fd4a57ac1a8
HT-Dat <hotiendat661@gmail.com>
2022-12-19 04:07:01 +0700

    Update UI for order form

--- src/components/Cards/OrderDetailCardTable.js
+++ src/components/Cards/OrderDetailCardTable.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Loading from "../../utils/Loading";
 import { getAllOrderDetails } from "../../features/order-details/orderDetailSlice";
 
-export const OrderDetailCardTable = ({ orderId }) => {
+export const OrderDetailCardTable = ({ orderId, discountedTotal, total }) => {
   const { orderDetails, isOrderDetailLoading, page, sort } = useSelector(
     (store) => store.orderDetail
   );
@@ -19,11 +19,7 @@ export const OrderDetailCardTable = ({ orderId }) => {
 
   return (
     <>
-      <div
-        className={
-          "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded bg-white"
-        }
-      >
+      <div className="flex flex-col min-w-0 break-words shadow-lg rounded">
         {orderDetails.length === 0 ? (
           <div className="block w-full overflow-x-auto">
             <h2 className="text-center pb-3">
@@ -36,33 +32,85 @@ export const OrderDetailCardTable = ({ orderId }) => {
             <table className="items-center w-full bg-transparent border-collapse">
               <thead>
                 <tr>
-                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100">
-                    Tên sản phẩm
+                  <th className="px-4 py-2 text-xs text-gray-500 ">#</th>
+                  <th className="px-4 py-2 text-xs text-gray-500 ">TÊN</th>
+                  <th className="px-4 py-2 text-xs text-gray-500 ">
+                    SỐ LƯỢNG SẢN PHẨM
                   </th>
-                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100">
-                    Số lượng sản phẩm
+                  <th className="px-4 py-2 text-xs text-gray-500 ">
+                    GIÁ ĐƠN SẢN PHẨM
                   </th>
-                  <th className="px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left bg-blueGray-50 text-blueGray-500 border-blueGray-100">
-                    Giá sản phẩm
-                  </th>
+                  <th className="px-4 py-2 text-xs text-gray-500 ">TỔNG PHỤ</th>
                 </tr>
               </thead>
               <tbody>
-                {orderDetails.map((orderDetail) => {
+                {orderDetails.map((orderDetail, index) => {
                   return (
-                    <tr key={orderDetail.id}>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-base whitespace-nowrap p-4">
-                        {orderDetail.productName}
+                    //     <tr className="whitespace-nowrap">
+                    //   <td className="px-6 py-4 text-sm text-gray-500">1</td>
+                    //   <td className="px-6 py-4">
+                    //     <div className="text-sm text-gray-900">
+                    //       Amazon Brand - Symactive Men's Regular Fit T-Shirt
+                    //     </div>
+                    //   </td>
+                    //   <td className="px-6 py-4">
+                    //     <div className="text-sm text-gray-500">4</div>
+                    //   </td>
+                    //   <td className="px-6 py-4 text-sm text-gray-500">$20</td>
+                    //   <td className="px-6 py-4">$30</td>
+                    // </tr>
+                    <tr className="whitespace-nowrap" key={orderDetail.id}>
+                      <td className="px-6 py-4 text-sm text-gray-500">
+                        {index + 1}
                       </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-base whitespace-nowrap p-4">
-                        {orderDetail.quantity}
+                      <td className="px-6 py-4">
+                        <div className="text-sm text-gray-900">
+                          {orderDetail.productName}
+                        </div>
                       </td>
-                      <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-base whitespace-nowrap p-4">
+                      <td className="px-6 py-4">
+                        <div className="text-sm text-gray-500">
+                          {orderDetail.quantity}
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 text-sm text-gray-500">
                         {orderDetail.price}
+                      </td>
+                      <td className="px-6 py-4 text-sm text-gray-500">
+                        {orderDetail.price * orderDetail.quantity}
                       </td>
                     </tr>
                   );
                 })}
+                <tr className="">
+                  <td colspan="3"></td>
+                  <td className="text-sm font-bold">Tổng cộng</td>
+                  <td className="text-sm font-bold tracking-wider">
+                    <b>{total}</b>
+                  </td>
+                </tr>
+                <tr>
+                  <th colspan="3"></th>
+                  <td className="text-sm font-bold">
+                    <b></b>
+                  </td>
+                  <td className="text-sm font-bold">
+                    <b>
+                      {discountedTotal !== total
+                        ? "Có giảm giá"
+                        : "Không giảm giá"}
+                    </b>
+                  </td>
+                </tr>
+                <tr className="text-white bg-gray-800">
+                  <th colspan="3"></th>
+                  <td className="text-sm font-bold">
+                    <b>Tổng cần trả</b>
+                  </td>
+                  <td className="text-sm font-bold">
+                    <b>{discountedTotal}</b>
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
